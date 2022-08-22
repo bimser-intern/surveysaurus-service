c98d34224638f6865b77bc81f3ef21c10dfe3241
Muhammed Yurga <101352337+MuhammedYurga@users.noreply.github.com>
2022-08-22 18:47:18 +0300

    bugfix

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router()
 const user = require('./user')
 const survey =require('./survey')
+const profile = require('./profile')
 
 router.use('/user', user)
 router.use('/survey', survey)
+router.use('/profile', profile)
 
 module.exports = router
